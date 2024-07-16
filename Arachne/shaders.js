9540e6a441ce44f922b9c40e820f7b19807be522
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2024-07-16 04:07:34 -0400

    Add Arachne

---
+++ Arachne/shaders.js
@@ -0,0 +1,216 @@
+let smoothLine = new ShaderProgram("smooth-line");
+
+smoothLine.vertText = `
+    // beginGLSL
+    #define pi 3.1415926535897932384626433832795
+    attribute float index;
+    attribute vec4 coordinates;
+    attribute vec4 color;
+    attribute float width;
+    attribute vec2 uv;
+    uniform vec2 resolution;
+    uniform float time;
+    varying vec4 c;
+    varying vec2 uvs;
+    varying vec2 wh;
+    varying float t;
+    void main(void) {
+        float ratio = (resolution.y / resolution.x);
+        vec2 pos = vec2(0., 0.);
+        vec2 pos0 = coordinates.xy;
+        vec2 pos1 = coordinates.zw;
+        // pos0 += vec2(
+        //     cos(pos0.x*pos0.y*4.+time*0.1*sign(pos0.x*pos0.y*4.)), 
+        //     sin(pos1.x*pos1.y*4.+time*0.1*sign(pos1.x*pos1.y*4.)))*0.01;
+        // pos1 += vec2(
+        //     cos(pos0.x*pos0.y*4.+time*0.1*sign(pos0.x*pos0.y*4.)), 
+        //     sin(pos1.x*pos1.y*4.+time*0.1*sign(pos1.x*pos1.y*4.)))*0.01;
+        // pos0 += vec2(
+        //     cos(pos0.x*pos0.y*400.+time*1.1*sign(pos.x*pos0.y*400.)), 
+        //     sin(pos1.x*pos1.y*400.+time*1.1*sign(pos.x*pos0.y*400.)))*0.0025;
+        // pos1 += vec2(
+        //     cos(pos0.x*pos0.y*400.+time*1.1*sign(pos.x*pos1.y*400.)), 
+        //     sin(pos1.x*pos1.y*400.+time*1.1*sign(pos.x*pos1.y*400.)))*0.0025;
+        float a = atan(pos1.y - pos0.y, pos1.x - pos0.x);
+        float pi75 = pi * 0.75;
+        float pi25 = pi * 0.25;
+        if (index == 0.) {
+            pos = pos0 + vec2(cos(a + pi75), sin(a + pi75)) * width;
+        } else if (index == 1.) {
+            pos = pos0 + vec2(cos(a - pi75), sin(a - pi75)) * width;
+        } else if (index == 2.) {
+            pos = pos1 + vec2(cos(a - pi25), sin(a - pi25)) * width;
+        } else if (index == 3.) {
+            pos = pos1 + vec2(cos(a + pi25), sin(a + pi25)) * width;
+        }
+        pos.x *= ratio;
+        gl_Position = vec4(pos, 0.0, 1.0);
+        wh = vec2(width * sin(pi75), length(pos1 - pos0));
+        c = color;
+        uvs = uv;
+        t = time;
+    }
+    // endGLSL
+`;
+smoothLine.fragText = `
+    // beginGLSL
+    precision mediump float;
+    varying vec4 c;
+    varying vec2 uvs;
+    varying vec2 wh;
+    varying float t;
+    float rand(vec2 co){
+        return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453 * (2.0 + sin(co.x)));
+    }
+    float map(float value, float min1, float max1, float min2, float max2) {
+        return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
+    }
+    void main(void) {
+        vec2 fc = gl_FragCoord.xy;
+        vec2 pos = gl_PointCoord;
+        float rando = rand(pos);
+        vec2 fwh = vec2(wh.x*2., wh.y+(wh.x*2.));
+        vec2 uv = uvs * fwh;
+        uv -= fwh * 0.5;
+        float radius = wh.x;
+        vec2 size = fwh * 0.5 - radius;
+        radius *= 2.;
+        float col = length(max(abs(uv), size) - size) - radius;
+        col = min(col * -1. * (1. / radius), 1.0);
+        col = pow(col, 3.) * 0.75 + pow(col, 43.);
+        col = smoothstep(0., 1., col);
+        // col = mix(pow(col, 10.)*0.25, col, sin(time*0.1+pos.y*0.5e1)*0.5+0.5);
+                col = mix(pow(col, 2.)*0.5, col, sin(t*0.1+pos.y*0.5e1)*0.5+0.5);
+               // col = mix(pow(col, 10.)*0.2, col, sin(t*0.1+pos.y*0.5e1)*0.5+0.5);
+        gl_FragColor = vec4(c.rgb, c.a * (max(col, 0.) - (rando * 0.1)));
+        gl_FragColor.g = pow(col, 2.) *  0.2;
+        gl_FragColor.b = pow(col, 2.) *  0.3;
+        // gl_FragColor.a = min(1., gl_FragColor.a + pow(col, 2.) *  0.25);
+        // gl_FragColor.rgb = gl_FragColor.gbr;
+    }
+    // endGLSL
+`;
+smoothLine.vertText = smoothLine.vertText.replace(/[^\x00-\x7F]/g, "");
+smoothLine.fragText = smoothLine.fragText.replace(/[^\x00-\x7F]/g, "");
+smoothLine.init();
+if (shadersReadyToInitiate) {
+    currentProgram = getProgram("smooth-line");
+    gl.useProgram(currentProgram);
+}
+
+
+let smoothDots = new ShaderProgram("smooth-dots");
+
+// Spatially fluctuating smoothDots
+smoothDots.vertText = `
+    // beginGLSL
+    attribute vec2 coordinates;
+    uniform float time;
+    varying float t;
+    float roundedRectangle (vec2 uv, vec2 pos, vec2 size, float radius, float thickness) {
+        float d = length(max(abs(uv - pos),size) - size) - radius;
+        return smoothstep(0.66, 0.33, d / thickness * 5.0);
+    }
+    void main(void) {
+        gl_Position = vec4(coordinates.x, coordinates.y, 0.0, 1.0);
+        gl_PointSize = 24.;
+        gl_PointSize += (sin((coordinates.y*0.02+time*2e-1))*0.5+0.5)*4.;
+    // gl_PointSize *= (sin(time*0.1+gl_Position.y*1e-2)*0.5+0.5);
+        t = time;
+        }
+    // endGLSL
+`;
+smoothDots.fragText = `
+    // beginGLSL
+    precision mediump float;
+    // uniform float time;
+    varying float t;
+    float rand(vec2 co){
+        return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453 * (2.0 + sin(co.x)));
+    }
+    void main(void) {
+        vec2 pos = gl_PointCoord;
+        float distSquared = 1.0 - dot(pos - 0.5, pos - 0.5) * 0.5;
+        float l = 1.0 - length(pos - vec2(0.5)) * 4.;
+        // l += (1.0 - length(pos - vec2(0.5)) * 2.) * 0.125;
+        // l += distSquared * 0.25;
+        distSquared -= 1.2;
+        l += (distSquared - (l * distSquared));
+        float halo = (1.0 - length(pos - vec2(0.5)) * 2.)*0.5;
+        l = smoothstep(0., 1., l);
+        l = mix(pow(l, 10.), l, (sin(t*0.1+gl_FragCoord.y*1e-2)*0.5+0.5));
+        float noise = rand(pos - vec2(cos(t), sin(t))) * 0.0625;
+        gl_FragColor = vec4(vec3(1.0, pow(l, 2.)*0.75, 0.25), l+halo-noise);
+        gl_FragColor.a *= (sin(t*0.1+gl_FragCoord.y*1e-2)*0.5+0.5);
+    }
+    // endGLSL
+`;
+smoothDots.vertText = smoothDots.vertText.replace(/[^\x00-\x7F]/g, "");
+smoothDots.fragText = smoothDots.fragText.replace(/[^\x00-\x7F]/g, "");
+smoothDots.init();
+
+smoothDots.vertText = `
+    // beginGLSL
+    attribute vec2 coordinates;
+    uniform float time;
+    uniform vec2 resolution;
+    varying float t;
+    varying vec2 posxy;
+    float roundedRectangle (vec2 uv, vec2 pos, vec2 size, float radius, float thickness) {
+        float d = length(max(abs(uv - pos),size) - size) - radius;
+        return smoothstep(0.66, 0.33, d / thickness * 5.0);
+    }
+    void main(void) {
+        vec2 pos = coordinates.xy;
+        // pos += vec2(
+        //     cos(pos.x*pos.y*2.+time*0.1), 
+        //     sin(pos.x*pos.y*2.+time*0.1)
+        //     )*0.01;
+        // pos += vec2(
+        //     cos(pos.x*pos.y*400.+time*1.1*sign(pos.x*pos.y*400.)), 
+        //     sin(pos.x*pos.y*400.+time*1.1*sign(pos.x*pos.y*400.)))*0.0025;
+        pos.x *= resolution.y / resolution.x;
+        gl_Position = vec4(pos.x, pos.y, 0.0, 1.0);
+        gl_PointSize = 20.;
+        // gl_PointSize += (sin((coordinates.y*0.02+time*2e-1))*0.5+0.5)*4.;
+        posxy = gl_Position.xy;
+        t = time;
+        
+    }
+    // endGLSL
+`;
+smoothDots.fragText = `
+    // beginGLSL
+    precision mediump float;
+    // uniform float time;
+    varying float t;
+    varying vec2 posxy;
+    float rand(vec2 co){
+        return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453 * (2.0 + sin(co.x)));
+    }
+    void main(void) {
+        vec2 pos = gl_PointCoord;
+        float distSquared = 1.0 - dot(pos - 0.5, pos - 0.5) * 0.5;
+        float l = 1.0 - length(pos - vec2(0.5)) * 4.;
+        // l += (1.0 - length(pos - vec2(0.5)) * 2.) * 0.125;
+        // l += distSquared * 0.25;
+        distSquared -= 1.2;
+        l += (distSquared - (l * distSquared));
+        float halo = (1.0 - length(pos - vec2(0.5)) * 2.)*0.5;
+        l = smoothstep(0., 1., l);
+        // l = smoothstep(0., 1., l);
+                // l = mix(pow(l, 10.)*0.0001, l, sin(t*0.1+posxy.y*0.125e1)*0.5+0.5);
+                // l = mix(pow(l, 10.)*0.0001, l, sin(t*0.1+posxy.y*0.125e1)*0.5+0.5);
+        
+        float instability = sin(t*0.1+posxy.y*0.25e1)*0.5+0.5;
+        float vanish = length((posxy-vec2(0.0, -0.4))*4.);
+        // vanish = floor(vanish);
+        vanish = min(pow(vanish, 2.0)*2.5, 1.);
+        float noise = rand(pos - vec2(cos(t), sin(t))) * 0.0625;
+        gl_FragColor = vec4(vec3(1.0, pow(l, 2.)*0.75, 0.25)-instability, (l+halo-noise)*0.5*vanish);
+    }
+    // endGLSL
+`;
+smoothDots.vertText = smoothDots.vertText.replace(/[^\x00-\x7F]/g, "");
+smoothDots.fragText = smoothDots.fragText.replace(/[^\x00-\x7F]/g, "");
+smoothDots.init();
---
+++ Arachne/sketch.js
@@ -0,0 +1,398 @@
+let looping = false;
+let keysActive = true;
+let socket, cnvs, ctx, canvasDOM;
+let fileName = "./frames/sketch";
+let maxFrames = 20;
+let gl, currentProgram;
+let vertices = [];
+let colors = [];
+let indices = [];
+let amountOfLines = 0;
+let drawCount = 0;
+let vertex_buffer, indices2_buffer, Index_Buffer, color_buffer, width_buffer, uv_buffer, dots_buffer;
+let dots = [];
+let frame = 0;
+let frameInc = Math.PI * 2 / 60;
+
+function setup() {
+    socket = io.connect('http://localhost:8080');
+    pixelDensity(1);
+    // cnvs = createCanvas(windowWidth, windowWidth / 16 * 9, WEBGL);
+    noCanvas();
+    cnvs = document.getElementById('my_Canvas');
+    gl = cnvs.getContext('webgl', { preserveDrawingBuffer: true });
+    // canvasDOM = document.getElementById('my_Canvas');
+    // canvasDOM = document.getElementById('defaultCanvas0');
+    // gl = canvasDOM.getContext('webgl');
+    // gl = cnvs.drawingContext;
+
+    // gl = canvasDOM.getContext('webgl', { premultipliedAlpha: false });
+
+    vertex_buffer = gl.createBuffer();
+    indices2_buffer = gl.createBuffer();
+    Index_Buffer = gl.createBuffer();
+    color_buffer = gl.createBuffer();
+    width_buffer = gl.createBuffer();
+    uv_buffer = gl.createBuffer();
+    dots_buffer = gl.createBuffer();
+    shadersReadyToInitiate = true;
+    initializeShaders();
+    currentProgram = getProgram("smooth-line");
+    gl.useProgram(currentProgram);
+
+    // gl.colorMask(false, false, false, true);
+    // gl.colorMask(false, false, false, true);
+
+    // Clear the canvas
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    // Enable the depth test
+    // gl.enable(gl.DEPTH_TEST);
+    gl.depthMask(false);
+
+    // Clear the color buffer bit
+    // gl.clear(gl.COLOR_BUFFER_BIT);
+    // gl.colorMask(true, true, true, true);
+    // gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.enable(gl.BLEND);
+    // gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    // gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    gl.blendFuncSeparate(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA, gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
+    // gl.blendFunc(gl.SRC_ALPHA, gl.DST_ALPHA);
+    // gl.blendFunc(gl.SRC_ALPHA, gl.DST_ALPHA);
+
+    // Set the view port
+    gl.viewport(0, 0, cnvs.width, cnvs.height);
+
+
+    frameRate(30);
+    background(0);
+    fill(255, 50);
+    noStroke();
+    if (!looping) {
+        noLoop();
+    }
+    setTimeout(function() {
+        scdConsoleArea.setAttribute("style", "display:block;");
+        scdArea.style.display = "none";
+        scdConsoleArea.setAttribute("style", "display:none;");
+        jsCmArea.style.height = "685px";
+        jsArea.style.display = "block";
+        displayMode = "js";
+        javaScriptEditor.cm.refresh();
+    }, 1);
+    setTimeout( function() {
+        keysControl.addEventListener("mouseenter", function(event) {
+            document.body.style.cursor = "none";
+            document.body.style.backgroundColor = "#000000";
+            appControl.setAttribute("style", "display:none;");
+            let tabs = document.querySelector("#file-tabs");
+            tabs.setAttribute("style", "display:none;");
+            cinemaMode = true;
+            scdArea.style.display = "none";
+            scdConsoleArea.style.display = "none";
+            jsArea.style.display = "none";
+            jsConsoleArea.style.display = "none";
+        }, false);
+        keysControl.addEventListener("mouseleave", function(event) {
+            if (!grimoire) {
+                document.body.style.cursor = "default";
+                document.body.style.backgroundColor = "#1C1C1C";
+                appControl.setAttribute("style", "display:block;");
+                let tabs = document.querySelector("#file-tabs");
+                tabs.setAttribute("style", "display:block;");
+                if (displayMode === "both") {
+                    scdArea.style.display = "block";
+                    scdConsoleArea.style.display = "block";
+                    jsArea.style.display = "block";
+                    jsConsoleArea.style.display = "block";
+                } else if (displayMode == "scd") {
+                    scdArea.style.display = "block";
+                    scdConsoleArea.style.display = "block";
+                } else if (displayMode == "js") {
+                    jsArea.style.display = "block";
+                    jsConsoleArea.style.display = "block";
+                }
+                cinemaMode = false;
+                clearSelection();
+            }   
+        }, false);
+    }, 1);
+}
+
+draw = function() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    resetLines();
+    resetDots();
+    drawArachne();
+    currentProgram = getProgram("smooth-dots");
+    gl.useProgram(currentProgram);
+    drawDots(currentProgram);
+    currentProgram = getProgram("smooth-line");
+    gl.useProgram(currentProgram);
+    drawLines();
+    if (exporting && frameCount < maxFrames) {
+        frameExport();
+    }
+    drawCount++;
+}
+
+resetLines = function() {
+    indices = [];
+    indices2 = [];
+    vertices = [];
+    colors = [];
+    widths = [];
+    uvs = [];
+    lineAmount = 0;
+};
+
+addLine = function(x0, y0, x1, y1, w, r, g, b, a) {
+    let ii = [0, 1, 2, 0, 2, 3];
+    let iii = [0, 1, 2, 3];
+    for (let k = 0; k < ii.length; k++) {
+        indices.push(ii[k] + (lineAmount*4));
+    }        
+    for (let k = 0; k < iii.length; k++) {
+        indices2.push(iii[k]);
+    }
+    let vv = [
+        x0, y0, x1, y1,
+        x0, y0, x1, y1,
+        x0, y0, x1, y1,
+        x0, y0, x1, y1
+    ];
+    for (let k = 0; k < vv.length; k++) {
+        vertices.push(vv[k]);
+    }
+    let cc = [
+        r, g, b, a, 
+        r, g, b, a, 
+        r, g, b, a, 
+        r, g, b, a
+    ];
+    for (let k = 0; k < cc.length; k++) {
+        colors.push(cc[k]);
+    }
+    widths.push(w, w, w, w);
+    let uv = [
+        0, 0, 
+        1, 0, 
+        1, 1, 
+        0, 1
+    ];
+    for (let k = 0; k < uv.length; k++) {
+        uvs.push(uv[k]);
+    }
+    lineAmount++;
+};
+
+
+drawLines = function() {
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, indices2_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(indices2), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, width_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(widths), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, uv_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uvs), gl.STATIC_DRAW); 
+    // setShaders();
+    /* ======== Associating shaders to buffer objects =======*/
+    // Bind vertex buffer object
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+    // Bind index buffer object
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
+    // Get the attribute location
+    var coord = gl.getAttribLocation(currentProgram, "coordinates");
+    // point an attribute to the currently bound VBO
+    gl.vertexAttribPointer(coord, 4, gl.FLOAT, false, 0, 0);
+    // Enable the attribute
+    gl.enableVertexAttribArray(coord);
+    // bind the indices2 buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, indices2_buffer);
+    // get the attribute location
+    var indices2AttribLocation = gl.getAttribLocation(currentProgram, "index");
+    // point attribute to the volor buffer object
+    gl.vertexAttribPointer(indices2AttribLocation, 1, gl.FLOAT, false, 0, 0);
+    // enable the color attribute
+    gl.enableVertexAttribArray(indices2AttribLocation);
+    // bind the color buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+    // get the attribute location
+    var color = gl.getAttribLocation(currentProgram, "color");
+    // point attribute to the volor buffer object
+    gl.vertexAttribPointer(color, 4, gl.FLOAT, false, 0, 0);
+    // enable the color attribute
+    gl.enableVertexAttribArray(color);
+    // bind the width buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, width_buffer);
+    // get the attribute location
+    var widthAttribLocation = gl.getAttribLocation(currentProgram, "width");
+    // point attribute to the volor buffer object
+    gl.vertexAttribPointer(widthAttribLocation, 1, gl.FLOAT, false, 0, 0);
+    // enable the color attribute
+    gl.enableVertexAttribArray(widthAttribLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, uv_buffer);
+    var uvAttribLocation = gl.getAttribLocation(currentProgram, "uv");
+    // point attribute to the volor buffer object
+    gl.vertexAttribPointer(uvAttribLocation, 2, gl.FLOAT, false, 0, 0);
+    // enable the color attribute
+    gl.enableVertexAttribArray(uvAttribLocation);
+    resolutionUniformLocation = gl.getUniformLocation(currentProgram, "resolution");
+    gl.uniform2f(resolutionUniformLocation, cnvs.width, cnvs.height);    
+    timeUniformLocation = gl.getUniformLocation(currentProgram, "time");
+    gl.uniform1f(timeUniformLocation, drawCount);
+    // gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+};
+
+let makeLine2 = function(x0, y0, x1, y1, w) {
+    let a0 = Math.atan2(y1 - y0, x1 - x0);
+    let halfPI = Math.PI * 0.5;
+    let c0 = Math.cos(a0 + halfPI) * w;
+    let c1 = Math.cos(a0 - halfPI) * w;
+    let s0 = Math.sin(a0 + halfPI) * w;
+    let s1 = Math.sin(a0 - halfPI) * w;
+    let xA = x0 + c0;
+    let yA = y0 + s0;
+    let xB = x0 + c1;
+    let yB = y0 + s1;
+    let xC = x1 + c0;
+    let yC = y1 + s0;
+    let xD = x1 + c1;
+    let yD = y1 + s1;
+    return [xA, yA, xB, yB, xC, yC, xD, yD];
+};
+
+resetDots = function() {
+    dots = [];
+};
+
+drawDots = function(selectedProgram) {
+    // vertices = [];
+    // num=0;
+    // for (let i = 0; i < reached.length; i++) {
+    //     vertices.push(reached[i][0], reached[i][1]);
+    //     num++;
+    // }
+    gl.bindBuffer(gl.ARRAY_BUFFER, dots_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(dots), gl.STATIC_DRAW);
+    // Get the attribute location
+    var coord = gl.getAttribLocation(selectedProgram, "coordinates");
+    // Point an attribute to the currently bound VBO
+    gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0);
+    // Enable the attribute
+    gl.enableVertexAttribArray(coord);
+    let timeUniformLocation = gl.getUniformLocation(selectedProgram, "time");
+    gl.uniform1f(timeUniformLocation, drawCount);
+    let resolutionUniformLocation = gl.getUniformLocation(selectedProgram, "resolution");
+    gl.uniform2f(resolutionUniformLocation, cnvs.width, cnvs.height);
+    gl.drawArrays(gl.POINTS, 0, dots.length * 0.5);
+};
+
+drawArachne = function() {
+    let a = 4;
+    let b = 3.2;
+    let r = 48 / cnvs.width * 3;
+    let br = (24 * 6) / cnvs.width * 3;
+    // stroke(255, 0, 0);
+    let ii = 0;
+    for (let i = 0; i < Math.PI * 2; i += Math.PI * 2 / 12) {
+        if (ii !== 7 && ii !== 5 && ii !== 6 && ii !== 0) {
+            let f = i;
+            let t = frame % (Math.PI * 2);
+            let x = (b + a * Math.cos(f + Math.sin(i * 2 + t) * 0.2)) * Math.cos(f + Math.sin(i * 2 + t) * 0.2) * r;
+            let y = (b + a * Math.cos(f + Math.sin(i * 2 + t) * 0.2)) * Math.sin(f + Math.sin(i * 2 + t) * 0.2) * r;
+            let cx = 0; let cy = 0;
+            let rr = rotate2D(0, 0, x, y, Math.PI * 0.5);
+            // dots.push(rr[0]*2, ((rr[1] + (br * 2))*-1+0.45)*2-0.25);
+            let bx = Math.cos(i - Math.PI * 0.5 + Math.sin(i * 2 + t) * 0.2) * br;
+            let by = Math.sin(i - Math.PI * 0.5 + Math.sin(i * 2 + t) * 0.2) * br;
+            addLine(
+                0, 0-0.25, bx*2, -by*2-0.25, 
+                1/2,
+                1, 0, 0, 0.5
+            );
+            addLine(
+                rr[0]*2, ((rr[1] + (br * 2))*-1+0.45)*2-0.25, bx*2, -by*2-0.25,
+                1/2,
+                1, 0, 0, 0.5
+            );
+            addLine(
+                0, 0-0.3, bx*2, -by*2-0.25, 
+                1/5,
+                1, 0, 0, 1
+            );
+            addLine(
+                rr[0]*2, ((rr[1] + (br * 2))*-1+0.45)*2-0.25, bx*2, -by*2-0.25,
+                1/5,
+                1, 0, 0, 1
+            );
+        }
+    ii++;
+    }
+    for (let i = 0; i < Math.PI * 2 - (Math.PI * 2 / 1200); i += Math.PI * 2 / 400) {
+        let f = i;
+        let x = (b + a * Math.cos(f)) * Math.cos(f) * r;
+        let y = (b + a * Math.cos(f)) * Math.sin(f) * r;
+        let cx = 0; let cy = 0;
+        let rr = rotate2D(0, 0, x, y, Math.PI * 0.5);
+        dots.push(rr[0]*2, ((rr[1] + (br*2))*-1+0.45)*2-0.25);
+    }
+    for (let i = 0; i < Math.PI * 2 - (Math.PI * 2 / 1200); i += Math.PI * 2 / 200) {
+        let bx = Math.cos(i - Math.PI * 0.5) * br * (2/3);
+        let by = Math.sin(i - Math.PI * 0.5) * br * (2/3);
+        dots.push(bx*2*1.5, -by*2*1.5-0.25);
+        dots.push(0+(Math.sin(i+drawCount*2e-1)*(i*0.005)), i*0.25-0.25);
+    }
+    // for (let i = 0; i < Math.PI * 2 - (Math.PI * 2 / 1200); i += Math.PI * 2 / 75) {
+        // let bx = Math.cos(i - Math.PI * 0.5) * br * (2/3);
+        // let by = Math.sin(i - Math.PI * 0.5) * br * (2/3);
+        // dots.push(bx * (3/2)*0.25*2, ((by * (3/2)*0.25 - (br*2) * 1.5)*-1-0.45*1.5)*2-0.3);
+        // ii++;
+    // }
+    frame += frameInc;
+};
+
+function rotate2D(cx, cy, x, y, angle) {
+    var rcos = Math.cos(angle),
+        rsin = Math.sin(angle),
+        nx = (rcos * (x - cx)) + (rsin * (y - cy)) + cx,
+        ny = (rcos * (y - cy)) - (rsin * (x - cx)) + cy;
+    return [nx, ny];
+}
+
+function rotate(p, a) {
+    return [
+        p.x * a.y + p.y * a.x,
+        p.y * a.y - p.x * a.x
+    ];
+}
+
+function keyPressed() {
+    if (keysActive) {
+        if (keyCode === 32) {
+            if (looping) {
+                noLoop();
+                looping = false;
+            } else {
+                loop();
+                looping = true;
+            }
+        }
+        if (key == 'p' || key == 'P') {
+            makeField();
+        }
+        if (key == 'r' || key == 'R') {
+            window.location.reload();
+        }
+        if (key == 'm' || key == 'M') {
+            redraw();
+        }
+    }
+}
